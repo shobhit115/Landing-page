971838afdcdab41728a92afab2363a628c8a8418
Shobhit Singh <112196213+shobhit115@users.noreply.github.com>
2025-08-23 02:28:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 export default function App() {
   return (
     <main>
-      <img className="absolute top-0 right-0 opacity-40 -z-10" src="src\assets\gradient.png" alt="Gradient" />
+      <img className="absolute top-0 right-0 opacity-40 -z-10" src="\gradient.png" alt="Gradient" />
       <Header/>
       <Hero />
     </main>
