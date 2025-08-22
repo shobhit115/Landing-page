228eeeb93f7de818a33a196073a3ceb349e6fd0f
Shivamkumar339 <shivam63395@gmail.com>
2025-08-23 02:46:16 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import gradient from "./assets/gradient.png";
 export default function App() {
   return (
     <main>
-      <img className="absolute top-0 right-0 opacity-40 -z-10" src="src\assets\gradient.png" alt="Gradient" />
+      <img className="absolute top-0 right-0 opacity-40 -z-10" src={gradient} alt="Gradient" />
       <Header/>
       <Hero />
     </main>
